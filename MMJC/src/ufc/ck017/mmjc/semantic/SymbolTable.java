e39903fa393bfb1b9def35765350ffaabf1dcddd
araruna <araruna@gmail.com>
2010-04-12 20:40:58 +0000

    Finalização da interface da tabela de Símbolos necessária para a verificação de tipos.

--- MMJC/src/ufc/ck017/mmjc/semantic/SymbolTable.java
+++ MMJC/src/ufc/ck017/mmjc/semantic/SymbolTable.java
@@ -1,9 +1,20 @@
 package ufc.ck017.mmjc.semantic;
 
+import java.util.List;
+
+import ufc.ck017.mmjc.node.PType;
+import ufc.ck017.mmjc.node.TId;
+import ufc.ck017.mmjc.util.SemanticType;
+
 public interface SymbolTable {
-	// TODO Substituir os Object por Symbol e defini-la
-	public boolean isMethod(Object c, Object m);
-	public boolean isClass(Object s);
-	// Deve retornar o tipo INVALID se nao existir
-	public Object getType(Object c, Object m, Object s);
+	public boolean isMethod(TId obj, TId mname, List<PType> params);
+	public boolean isMethod(TId mname, List<PType> params);
+	public boolean isClass(TId cname);
+	public boolean isVar(TId id);
+	public boolean isSubclassOf(TId obj, TId cname);
+	
+	public SemanticType getType(TId id);
+	
+	public void enterScope(TId id);
+	public void exitScope();
 }
