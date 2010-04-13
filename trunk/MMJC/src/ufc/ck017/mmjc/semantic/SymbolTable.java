f35a7e8e1565b5869529373220accf30843a633f
araruna <araruna@be26690a-222c-11df-b10e-39eb892c48f2>
2010-04-13 04:48:33 +0000

    Mudanças na verificação semântica de tipos. Mudanças na interface da Tabela de Símbolos usada por essa fase. (Provavelmente alguns outros métodos surgirão e outros sumirão :D) Adição dos erros semânticos na classe SemanticError correspondentes às situações adversas possíveis até o momento.

--- trunk/MMJC/src/ufc/ck017/mmjc/semantic/SymbolTable.java
+++ trunk/MMJC/src/ufc/ck017/mmjc/semantic/SymbolTable.java
@@ -2,18 +2,30 @@ package ufc.ck017.mmjc.semantic;
 
 import java.util.List;
 
-import ufc.ck017.mmjc.node.PType;
 import ufc.ck017.mmjc.node.TId;
-import ufc.ck017.mmjc.util.SemanticType;
+import ufc.ck017.mmjc.symbolTable.TypeSymbol;
 
+// TODO verificar se todas as versões dos métodos são realmente necessárias.
+// ATENCAO COM AS ASSINATURAS QUE MUDARAM... ESPERO QUE TEJAM CERTAS... :(
 public interface SymbolTable {
-	public boolean isMethod(TId obj, TId mname, List<PType> params);
-	public boolean isMethod(TId mname, List<PType> params);
+	public boolean isMethod(TypeSymbol obj, TId mname, List<TypeSymbol> params);
+	public boolean isMethod(TId mname, List<TypeSymbol> params);
+	
 	public boolean isClass(TId cname);
+	public boolean isClass(TypeSymbol cname);
+	
 	public boolean isVar(TId id);
+	
+	public boolean isObject(TId id);
+	public boolean isObject(TypeSymbol id);
+	
+	public boolean isSubclassOf(TId obj, TypeSymbol cname);
 	public boolean isSubclassOf(TId obj, TId cname);
 	
-	public SemanticType getType(TId id);
+	
+	public TypeSymbol getType(TId id);
+	public TypeSymbol getType(TypeSymbol cname, TId id);
+	
 	
 	public void enterScope(TId id);
 	public void exitScope();
--- trunk/MMJC/src/ufc/ck017/mmjc/util/SemanticError.java
+++ trunk/MMJC/src/ufc/ck017/mmjc/util/SemanticError.java
@@ -4,27 +4,44 @@ import ufc.ck017.mmjc.node.AAtbStatement;
 import ufc.ck017.mmjc.node.AVatbStatement;
 import ufc.ck017.mmjc.node.PExpression;
 import ufc.ck017.mmjc.node.TId;
+import ufc.ck017.mmjc.symbolTable.TypeSymbol;
 
 public class SemanticError {
 	// TODO Melhorar as mensagens de erro, dizendo por exemplo os tipos esperados e os obtidos.
 	
 	public static String idNotFound(TId id) {
-		return "Semantic error at ["+id.getLine()+','+id.getPos()+":\n\tIdentifier \'"+id.getText()+"\' was not previously defined at this scope.";
+		return "Semantic error at ["+id.getLine()+','+id.getPos()+"]:\n\tIdentifier \'"+id.getText()+"\' was not previously defined at this scope.";
 	}
 	
 	public static String invalidAtb(AAtbStatement atbst) {
-		return "Semantic error at ["+atbst.getId().getLine()+','+atbst.getId().getPos()+":\n\tInvalid operation. Check the types of the identifier and the expression involved.";
+		return "Semantic error at ["+atbst.getId().getLine()+','+atbst.getId().getPos()+"]:\n\tInvalid assignment. Check the types of the identifier and the expression involved.";
 	}
 	
 	public static String invalidAtb(AVatbStatement atbst) {
-		return "Semantic error at ["+atbst.getId().getLine()+','+atbst.getId().getPos()+":\n\tInvalid operation. Check the types of the identifier, the index, and the expression involved.";
+		return "Semantic error at ["+atbst.getId().getLine()+','+atbst.getId().getPos()+"]:\n\tInvalid assignment. Check the types of the identifier (must be <int[]>), the index (must be <int>), and the expression involved (must be <int>).";
 	}
 	
-	public static String expectedExprType(PExpression expr, SemanticType expected, SemanticType got) {
-		return null;//"Semantic error at ["+expr.getLine()+','+expr.getPos()+":\n\tExpected "+expected+" expression but got "+got+" expression.";
+	public static String expectedExprType(PExpression expr, TypeSymbol expected, TypeSymbol got) {
+		return null;//"Semantic error at ["+expr.getLine()+','+expr.getPos()+"]:\n\tExpected <"+expected+"> type expression but got <"+got+"> type expression.";
 	}
 
-	public static String methodExpected(TId mname) {
-		return "Semantic error at ["+mname.getLine()+','+mname.getPos()+":\n\tIdentifier \'"+mname.getText()+"\' should name a method.";
+	public static String methodExpected(TypeSymbol cname, TId mname) {
+		return "Semantic error at ["+mname.getLine()+','+mname.getPos()+"]:\n\tIdentifier \'"+mname.getText()+"\' does not name a method of class <"+cname+">.";
+	}
+
+	public static String invalidAtbToObject(TId idst, TypeSymbol classtype) {
+		return "Semantic error at ["+idst.getLine()+','+idst.getPos()+"]:\n\tIdentifier has type <"+TypeSymbol.symbol(idst.getText())+"> but was assigned an incompatible value of type <"+classtype+">.";
+	}
+
+	public static String incompatibleClassAtb(TId idst, TypeSymbol classtype) {
+		return "Semantic error at ["+idst.getLine()+','+idst.getPos()+"]:\n\tThe class <"+TypeSymbol.symbol(idst.getText())+"> is not a superclass of the <"+classtype+"> class.";
+	}
+
+	public static String invalidTypePrint(PExpression expr) {
+		return null;//"Semantic error at ["+expr.getLine()+','+expr.getPos()+"]:\n\tExpected <"+TypeSymbol.getIntTSymbol()+"> expression but got <"+expr.getType()+"> type expression.";
+	}
+
+	public static String objectExpected(PExpression object) {
+		return null;//"Semantic error at ["+object.getLine()+','+object.getPos()+"]:\n\tExpected object or \'this\' expression, but got <"+object.getType()+"> type expression.";
 	}
 }
